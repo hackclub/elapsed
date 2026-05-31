90ea726dca2e0ed51f615084cbfe0ff9e431bf38
Lamparter <71598437+Lamparter@users.noreply.github.com>
2026-05-31 21:06:38 +0100

    Remove DI services
    [skip ci]

--- src/platforms/Riverside.Elapsed.App/Services/Api/IApiClientFacade.cs
+++
@@ -1,6 +0,0 @@
-namespace Riverside.Elapsed.App.Services.Api;
-
-public interface IApiClientFacade
-{
-	Task<T?> SendAsync<T>(Func<ApiClient, Task<T?>> call, CancellationToken cancellationToken = default);
-}
